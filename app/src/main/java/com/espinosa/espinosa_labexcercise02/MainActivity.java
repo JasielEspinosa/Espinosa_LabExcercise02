48bfee9720a08409e542a7c0c21e514a9418b21e
Jasiel Espinosa <jaszespi@gmail.com>
2017-10-26 16:56:32 +0800

    Logical state updated

--- app/src/main/java/com/espinosa/espinosa_labexcercise02/MainActivity.java
+++ app/src/main/java/com/espinosa/espinosa_labexcercise02/MainActivity.java
@@ -1,27 +1,73 @@
 package com.espinosa.espinosa_labexcercise02;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonNext;
+    Button buttonNext, buttonSP, buttonIS;
+    EditText inputUsername, inputPassword;
+    SharedPreferences preferences;
+    FileOutputStream fos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inputUsername = (EditText) findViewById(R.id.input_username);
+        inputPassword = (EditText) findViewById(R.id.input_password);
         buttonNext = (Button) findViewById(R.id.btn_next);
+        buttonSP = (Button) findViewById(R.id.btn_sharedPreferences);
+        buttonIS = (Button) findViewById(R.id.btn_internalStorage);
+
+        preferences = getPreferences(Context.MODE_PRIVATE);
 
     }
 
     public void callSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
+    }
+
+    public void triggerSharedPref(View view) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("usernameSP", inputUsername.getText().toString());
+        editor.putString("passwordSP", inputPassword.getText().toString());
+        editor.commit();
+        Toast.makeText(this, "Successfully saved to Shared Preferences", Toast.LENGTH_LONG).show();
+    }
+
+    public void triggerInternalStorage(View view) {
+        String usernameIS = inputUsername.getText().toString();
+        String passwordIS = inputPassword.getText().toString();
+        try {
+            fos = openFileOutput("output.txt", Context.MODE_PRIVATE);
+            fos.write(usernameIS.getBytes());
+            fos.write(passwordIS.getBytes());
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "Successfully saved to Internal Storage", Toast.LENGTH_LONG).show();
     }
 
 }
--- app/src/main/java/com/espinosa/espinosa_labexcercise02/SecondActivity.java
+++ app/src/main/java/com/espinosa/espinosa_labexcercise02/SecondActivity.java
@@ -1,22 +1,40 @@
 package com.espinosa.espinosa_labexcercise02;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SecondActivity extends AppCompatActivity {
 
-    Button buttonBack, buttonClear;
+    Button buttonBack, buttonClear, buttonLoadSP, buttonLoadIS;
+    TextView displayText;
+    SharedPreferences preferences;
+    FileInputStream fis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        buttonLoadSP = (Button) findViewById(R.id.btn_loadSharedPref);
+        buttonLoadIS = (Button) findViewById(R.id.btn_loadInternalStorage);
+
+        displayText = (TextView) findViewById(R.id.displayText);
+
+        buttonBack = (Button) findViewById(R.id.btn_back);
         buttonClear = (Button) findViewById(R.id.btn_clear);
 
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     public void callMainActivity(View view) {
@@ -24,5 +42,31 @@ public class SecondActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void loadSP(View view) {
+        String usernameSP = preferences.getString("usernameSP", "");
+        String passwordSP = preferences.getString("passwordSP", "");
+        displayText.setText("Username is " + usernameSP + " and Password is " + passwordSP);
+    }
+
+    public void loadIS(View view) {
+        StringBuffer buffer = new StringBuffer();
+        int read = 0;
+        try {
+            fis = openFileInput("output.txt");
+            while ((read = fis.read()) != -1) {
+                buffer.append((char) read);
+            }
+            fis.close();
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        displayText.setText(buffer.toString());
+    }
+
+    public void clearDisplay(View view){
+        displayText.setText("");
+    }
 
 }
